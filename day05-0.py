d8db34efa0ef7519ff8073e6382e864a61ff72dd
Miles K Rains <mileskrains@gmail.com>
2018-12-07 08:07:44 -0800

    passes part 2 test data, but not comp input. wtf

--- day05-0.py
+++ day05-0.py
@@ -1,51 +1,43 @@
-with open('input05-0.txt') as f:
-    polymer = f.read().strip()
+import string
 
-# polymer = ['xAAazZa', 'dabAcCaCBAcCcaDA'][0]
+
+with open('input05-0.txt') as f:
+    input_polymer = f.read().strip()
+
+polymer = [input_polymer, 'dabAcCaCBAcCcaDA'][1]
 
 ords = [ord(c) for c in polymer]
-size = len(ords)
-print(size)
+print(len(ords))
 
-use_version_2 = True
 
-while True:
-    if use_version_2:
-        break
-    for i in range(1, len(ords)):
-        if abs(ords[i] - ords[i - 1]) == 32:
-            ords[i] = 0
-            ords[i - 1] = 0
+def reacted_size(ords, dropchar='*'):
+    pos2 = 1
+    pos1 = 0
+
+    drop_ords = (ord(dropchar.lower()), ord(dropchar.upper()))
+    ords = [e for e in ords if e not in drop_ords]
+    size = len(ords)
+
+    while pos2 < size:
+        if abs(ords[pos2]-ords[pos1]) == 32:
+            ords[pos1] = 0
+            ords[pos2] = 0
+            pos2 += 1
+            pos1 -= 1
+            if pos1 < 0:
+                pos1 += 1
+            elif ords[pos1] == 0:
+                while ords[pos1] == 0:
+                    pos1 -= 1
+        else:
+            pos2 += 1
+            pos1 = pos2 - 1
+
     ords = [e for e in ords if e]
-    if len(ords) == size:
-        break
-    else:
-        size = len(ords)
+    return len(ords)
 
-# code below is more efficient, works for test input, but gives much higher answer for actual input
 
-pos2 = 1
-pos1 = 0
+print(reacted_size(ords))
 
-while use_version_2 and pos2 < size:
-    # print(ords, pos1, pos2)
-    if abs(ords[pos2]-ords[pos1]) == 32:
-        ords[pos1] = 0
-        ords[pos2] = 0
-        pos2 += 1
-        pos1 -= 1
-        if pos1 < 0:
-            pos1 += 1
-        elif ords[pos1] == 0:
-            while ords[pos1] == 0:
-                pos1 -= 1
-    else:
-        pos2 += 1
-        pos1 = pos2 - 1
-
-ords = [e for e in ords if e]
-size = len(ords)
-print(size)
-
-# print(''.join([chr(o) for o in ords]))
-
+dropsizes = [(reacted_size(ords, letter), letter) for letter in string.ascii_lowercase]
+print(sorted(dropsizes))
