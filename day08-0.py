bc10ceed32b2926c49901c0c1981ea7b2158ac32
Miles K Rains <mileskrains@gmail.com>
2018-12-14 22:19:39 -0800

    day 8 part 2 completed, yay generators and recursive functions

--- day08-0.py
+++ day08-0.py
@@ -1,6 +1,8 @@
 with open('input08-0.txt') as f:
     tree_data = [int(n) for n in f.readline().split()]
 
+# tree_data = [2, 3, 0, 3, 10, 11, 12, 1, 1, 0, 1, 99, 2, 1, 1, 2]
+
 kind_stack = ['header']
 metadata_sum = 0
 for n in tree_data:
@@ -17,3 +19,22 @@ for n in tree_data:
         metadata_sum += n
 
 print(metadata_sum)
+
+
+def sum_children():
+    child_ct = next(tree_iter)
+    meta_ct = next(tree_iter)
+    child_values = [sum_children() for _ in range(child_ct)]
+    meta_values = [next(tree_iter) for _ in range(meta_ct)]
+    if child_values:
+        chm_sum = 0
+        for m in meta_values:
+            if 0 < m <= len(child_values):
+                chm_sum += child_values[m - 1]
+        return chm_sum
+    else:
+        return sum(meta_values)
+
+
+tree_iter = iter(tree_data)
+print(sum_children())
