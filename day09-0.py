bc4ca9dbbf3031493cc9dbef546b8cd8fa5488c9
Miles K Rains <mileskrains@gmail.com>
2018-12-15 15:31:54 -0800

    day 9 part 1 complete

--- day09-0.py
+++ day09-0.py
@@ -1,20 +1,22 @@
-# sub 1: 436212, too high, runs slow (approx 15s)
-
-marbles = [0]
-cur_pos = 0
+marbles = [0, 2, 1]
+cur_pos = 1
 
 player_ct = 426
 player_scores = [0] * player_ct
 high_marble = 72058
 
-for m in range(1, high_marble+1):
+for m in range(3, high_marble + 1):
     cur_player = m % player_ct
     if m % 23 == 0:
-        cur_pos = (cur_pos - 7) % len(marbles) + 1
+        cur_pos -= 7
+        if cur_pos < 0:
+            cur_pos %= len(marbles)
         rem_val = marbles.pop(cur_pos)
-        player_scores[cur_player] += m + rem_val
+        player_scores[cur_player] += (m + rem_val)
     else:
-        cur_pos = (cur_pos + 2) % len(marbles)
-        marbles = marbles[:cur_pos+1]+[m]+marbles[cur_pos+1:]
+        cur_pos = cur_pos + 2
+        if cur_pos > len(marbles):
+            cur_pos %= len(marbles)
+        marbles.insert(cur_pos, m)
 
 print(max(player_scores))
