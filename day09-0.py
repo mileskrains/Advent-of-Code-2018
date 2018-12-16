f97f83a7dbc5d685685ac60e42d38f186ddb358f
Miles K Rains <mileskrains@gmail.com>
2018-12-15 22:01:17 -0800

    day 9 part 2 complete, changed to linked-list approach for reasonable runtime (and code clarity)

--- day09-0.py
+++ day09-0.py
@@ -20,3 +20,43 @@ for m in range(3, high_marble + 1):
         marbles.insert(cur_pos, m)
 
 print(max(player_scores))
+
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.cw = self
+        self.ccw = self
+
+    def insert_cw(self, value):
+        new_node = Node(value)
+        self.cw.ccw = new_node
+        new_node.cw = self.cw
+        new_node.ccw = self
+        self.cw = new_node
+        return new_node
+
+    def remove(self):
+        self.ccw.cw = self.cw
+        self.cw.ccw = self.ccw
+        return self.cw
+
+
+player_ct = 426
+player_scores = [0] * player_ct
+high_marble = 7205800
+
+cur_pos = Node(0)
+
+for m in range(1, high_marble + 1):
+    cur_player = m % player_ct
+    if m % 23 != 0:
+        cur_pos = cur_pos.cw.insert_cw(m)
+    else:
+        for _ in range(7):
+            cur_pos = cur_pos.ccw
+        score_incr = m + cur_pos.value
+        player_scores[cur_player] += score_incr
+        cur_pos = cur_pos.remove()
+
+print(max(player_scores))
